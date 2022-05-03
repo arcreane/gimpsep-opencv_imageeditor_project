6a011d075db03f21e4d21e4f23df3b95302e1bbc
Amanda Dieuaide <amanda.dieuaide@eleve.isep.fr>
2022-05-03 10:12:41 +0200

    Ajoutez des fichiers projet.

---
+++ OpenCV__ImageEditor_Project/Source.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main( int argc, char** argv )
+{
+ char* imageName = argv[1];
+
+ Mat image;
+ image = cv::imread("C:/Users/amand/Downloads/A2/MediaApp/AmandaDieuaide-TP2/HappyFish.jpg");
+ if( argc != 2 || !image.data )
+ {
+   printf( " No image data \n " );
+   return -1;
+ }
+ Mat gray_image;
+
+ cvtColor(image, gray_image, COLOR_BGR2GRAY);
+
+ namedWindow("WindowName");
+ namedWindow("WindowNameGray");
+
+ cv::imshow("WindowName", image);
+ cv::imshow("WindowNameGray", gray_image);
+ cv::imwrite("C:/Users/amand/Downloads/A2/MediaApp/AmandaDieuaide-TP2/Gray_HappyFish.jpg", gray_image);
+ waitKey(0);
+
+ return 0;
+}
