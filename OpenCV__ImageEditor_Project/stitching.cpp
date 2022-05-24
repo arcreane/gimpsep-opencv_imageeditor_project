c2db6334dc9f58d907d3cf439d264d06ba64d59e
Adriberger <74058506+Adriberger@users.noreply.github.com>
2022-05-24 12:13:00 +0200

    final

--- OpenCV__ImageEditor_Project/stitching.cpp
+++ OpenCV__ImageEditor_Project/stitching.cpp
@@ -1,166 +1,130 @@
-
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/stitching.hpp"
-#include <iostream>
-/*
-using namespace std;
-using namespace cv;
-bool divide_images = false;
-Stitcher::Mode mode = Stitcher::PANORAMA;
-vector<Mat> imgs;
-string result_name = "result.jpg";
-void printUsage(char** argv);
-int parseCmdArgs(int argc, char** argv);
-int main(int argc, char* argv[])
-{
-    int retval = parseCmdArgs(argc, argv);
-    if (retval) return EXIT_FAILURE;
-    Mat pano;
-    Ptr<Stitcher> stitcher = Stitcher::create(mode);
-    Stitcher::Status status = stitcher->stitch(imgs, pano);
-    if (status != Stitcher::OK)
-    {
-        cout << "Can't stitch images, error code = " << int(status) << endl;
-        return EXIT_FAILURE;
-    }
-    imwrite(result_name, pano);
-    cout << "stitching completed successfully\n" << result_name << " saved!";
-    return EXIT_SUCCESS;
-}
-void printUsage(char** argv)
-{
-    cout <<
-        "Images stitcher.\n\n" << "Usage :\n" << argv[0] << " [Flags] img1 img2 [...imgN]\n\n"
-        "Flags:\n"
-        "  --d3\n"
-        "      internally creates three chunks of each image to increase stitching success\n"
-        "  --mode (panorama|scans)\n"
-        "      Determines configuration of stitcher. The default is 'panorama',\n"
-        "      mode suitable for creating photo panoramas. Option 'scans' is suitable\n"
-        "      for stitching materials under affine transformation, such as scans.\n"
-        "  --output <result_img>\n"
-        "      The default is 'result.jpg'.\n\n"
-        "Example usage :\n" << argv[0] << " --d3 --mode scans img1.jpg img2.jpg\n";
-}
-int parseCmdArgs(int argc, char** argv)
-{
-    if (argc == 1)
-    {
-        printUsage(argv);
-        return EXIT_FAILURE;
-    }
-    for (int i = 1; i < argc; ++i)
-    {
-        if (string(argv[i]) == "--help" || string(argv[i]) == "/?")
-        {
-            printUsage(argv);
-            return EXIT_FAILURE;
-        }
-        else if (string(argv[i]) == "--d3")
-        {
-            divide_images = true;
-        }
-        else if (string(argv[i]) == "--output")
-        {
-            result_name = argv[i + 1];
-            i++;
-        }
-        else if (string(argv[i]) == "--mode")
-        {
-            if (string(argv[i + 1]) == "panorama")
-                mode = Stitcher::PANORAMA;
-            else if (string(argv[i + 1]) == "scans")
-                mode = Stitcher::SCANS;
-            else
-            {
-                cout << "Bad --mode flag value\n";
-                return EXIT_FAILURE;
-            }
-            i++;
-        }
-        else
-        {
-            Mat img = imread(samples::findFile(argv[i]));
-            if (img.empty())
-            {
-                cout << "Can't read image '" << argv[i] << "'\n";
-                return EXIT_FAILURE;
-            }
-            if (divide_images)
-            {
-                Rect rect(0, 0, img.cols / 2, img.rows);
-                imgs.push_back(img(rect).clone());
-                rect.x = img.cols / 3;
-                imgs.push_back(img(rect).clone());
-                rect.x = img.cols / 2;
-                imgs.push_back(img(rect).clone());
-            }
-            else
-                imgs.push_back(img);
-        }
-    }
-    return EXIT_SUCCESS;
-}*/
-
-
-/*
-
-// CPP program to Stitch
-// input images (panorama) using OpenCV 
 #include <iostream>
 #include <fstream>
-
-// Include header files from OpenCV directory
-// required to stitch images.
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/stitching.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/stitching.hpp>
 
 using namespace std;
 using namespace cv;
 
-// Define mode for stitching as panoroma 
-// (One out of many functions of Stitcher)
-Stitcher::Mode mode = Stitcher::PANORAMA;
+// Global variables.
+double minval, maxval;
+Point minloc, maxloc;
+Rect rect;
+Mat result;
+
+// Create function that finds point where overlap between two images starts. 
+// Images "left_img" and "right_img" are passed in by find_overlaps function.
+Point find_overlap_start(Mat left_img, Mat right_img)
+{
+    // Copy left image to new variable.
+    Mat img = left_img;
+
+    // Create matrix to be used as our "template". 
+    rect.width = (img.size().width) / 10;
+    rect.height = img.size().height;
+    Mat templ = right_img(rect);
+
+    // Apply template matching and store result.
+    matchTemplate(img, templ, result, TM_CCORR_NORMED);
+    minMaxLoc(result, &minval, &maxval, &minloc, &maxloc);
+
+    // Return top-left coordinate where the matching starts.
+    return maxloc;
+}
+
+// Create vector of points where images start to overlap.
+vector<Point> find_overlaps(vector<Mat> images)
+{
+    // Create vector of overlaps coordinates.
+    vector<Point> overlap_starts;
+
+    for (int i = 0; i != images.size() - 1; i++)
+    {
+        // Find overlap start between two images.
+        Point overlap = find_overlap_start(images[i], images[i + 1]);
+
+        // Add overlap point to vector of points.
+        overlap_starts.push_back(overlap);
+    }
+
+    // Return vector of "overlaping points" (top-left).
+    return overlap_starts;
+}
+
+// Simple stitch, no blending, right hand slice overlays left hand slice.
+Mat stitch_images(vector<Mat> images, vector<Point> overlap_starts)
+{
+
+    // Define variable to store width of the composite image.
+    int total_width = 0;
+
+    // Loop trought vector of overlaped points and calculate total_width.
+    for (int i = 0; i < images.size() - 1; i++)
+    {
+        // Take the x coordinates of Point in overlap_starts vector.
+        total_width += overlap_starts[i].x;
+
+    }
+
+    // Now add width of image to the total width.
+    int img_width = images[images.size() - 1].cols;
+    total_width = total_width + img_width;  
+
+    // Create matrix that is of the size ( height, total_width -> depends on the number of images).
+    Mat result = Mat::zeros(Size(total_width, images[1].rows), images[0].type());
+   
+    // Put images next to each other on the result matrix.
+    int current_column = 0;
+    for (int i = 0; i < images.size(); i++)
+    {
+        images[i].copyTo(result.rowRange(0, result.rows).colRange(current_column, (current_column + images[i].cols)));
+
+        // The next image should be moved right for:
+        if (i != images.size() - 1) current_column += overlap_starts[i].x;
+    }
+
+    // Display composite image.
+    namedWindow("result", WINDOW_AUTOSIZE);
+    imshow("result", result);
+    
+    return result;
+}
 
 
 int main(int argc, char* argv[])
 {
+    // Ask the user how many images to switch
+    int number_img;
+    cout << "How many images do you want to stitch? " << endl;
+    cin >> number_img;
 
-    std::cout << "ici" << std::endl;
-    // Array for pictures
-    vector<Mat> imgs;
-    Mat image;
-    image = cv::imread("C:/Users/adria/Documents/ISEP/A2/Dev/TP2/HappyFish.jpg");
-    imgs.push_back(image);
-
-    // Define object to store the stitched image
-    Mat pano;
-
-    // Create a Stitcher class object with mode panoroma
-    Ptr<Stitcher> stitcher = Stitcher::create(mode,false);
-
-    // Command to stitch all the images present in the image array
-    Stitcher::Status status = stitcher->stitch(imgs, pano);
-
-    if (status != Stitcher::OK)
+    // Create vector of images.
+    string path;
+    vector<Mat> various_images;
+    for (int i = 0; i < number_img; i++) 
     {
-        // Check if images could not be stiched
-        // status is OK if images are stiched successfully
-        cout << "Can't stitch images\n";
-        return -1;
+        // Load the image
+        cout << "Enter image path " << endl;
+        cin >> path;
+        Mat img = imread(path);
+
+        // Store the image in the vector
+        various_images.push_back(img);
     }
 
-    // Store a new image stiched from the given 
-    //set of images as "result.jpg"
-    imwrite("result.jpg", pano);
+    // Display loaded images.
+    for (int i = 0; i < number_img; i++) 
+    {
+        namedWindow("img " + std::to_string(i +1), WINDOW_AUTOSIZE);
+        imshow("img " + std::to_string(i + 1), various_images[i]);
+    }
 
-    // Show the result
-    imshow("Result", pano);
-
+    // Find top-left points of overlaping.
+    vector<Point> overlap_starts = find_overlaps(various_images);
+    
+    // Stitch images together.
+    stitch_images(various_images, overlap_starts);
+    
     waitKey(0);
     return 0;
 }
-
-*/
